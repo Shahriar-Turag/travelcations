f7e106a36db8c3e76dc62f51c34dc728b0e3d822
Shahria-Turag <shahriarturag.st@gmail.com>
2022-04-18 06:04:10 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./Components/Shared/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 
 function App() {
     return (
         <div className="App">
-            <Home></Home>
+            <Navbar />
         </div>
     );
 }
--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,9 +1,60 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>tour guide</h2>
+        <div className="navbar__container">
+            <nav className="navbar">
+                <div className="navbar__logo">
+                    <NavLink to="/">Travelcations</NavLink>
+                </div>
+
+                <div className="navbar__links">
+                    <ul>
+                        <li className="nav-link">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                                to="/"
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                                to="/blogs"
+                            >
+                                Blogs
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                                to="/about"
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                                to="/account"
+                            >
+                                Account
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 };
