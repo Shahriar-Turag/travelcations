e7b9c95b2eb3416ecefaee1fde7ebf6dd4371288
Shahria-Turag <shahriarturag.st@gmail.com>
2022-04-18 18:19:58 +0600

    add services, extra sections, footer, and accounts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./Components/Shared/Footer/Footer";
 import Navbar from "./Components/Shared/Navbar/Navbar";
+import AccountPage from "./Pages/AccountPage/AccountPage";
+import BlogPage from "./Pages/BlogPage/BlogPage";
 import Home from "./Pages/Home/Home";
 
 function App() {
     return (
-        <div className="App">
+        <>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/account" element={<AccountPage />} />
+                <Route path="/blogs" element={<BlogPage />} />
             </Routes>
-        </div>
+            <Footer />
+        </>
     );
 }
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Blogs from "../../Components/Constant/Blogs/Blogs";
 import Header from "../../Components/Constant/Header/Header";
+import Services from "../../Components/Constant/Services/Services";
 
 const Home = () => {
     return (
         <div className="page">
             <Header />
+            <Services />
+            <Blogs />
         </div>
     );
 };
