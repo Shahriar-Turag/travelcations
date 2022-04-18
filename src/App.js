e61d654beccb6738dd3e686ec17a28c151aa2300
Shahria-Turag <shahriarturag.st@gmail.com>
2022-04-18 06:38:07 +0600

    banner done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
@@ -6,6 +7,9 @@ function App() {
     return (
         <div className="App">
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../../Components/Constant/Header/Header";
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className="page">
+            <Header />
         </div>
     );
 };
